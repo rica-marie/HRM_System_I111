5934538f6c73a407daa7efe1b884df83fc6befcf
Missy Key Sadsad <88454597+xoxolrjj@users.noreply.github.com>
2024-12-05 04:37:51 +0800

    fix sign up

--- model/employee.php
+++ model/employee.php
@@ -11,22 +11,57 @@ class EmployeeModel
         }
     }
 
-    public function validateEmployee($username, $password)
-    { 
-        $query = "SELECT * FROM employee WHERE username = ? AND password = ?";
-        $stmt = $this->db->prepare($query);
+    // public function validateEmployee($username, $password)
+    // {
+    //     $query = "SELECT * FROM employee WHERE username = ? AND password = ?";
+    //     $stmt = $this->db->prepare($query);
         
-        if (!$stmt) {
-            die("SQL error: " . $this->db->error); // Debugging info
-        }
+    //     if (!$stmt) {
+    //         die("SQL error: " . $this->db->error); // Debugging info
+    //     }
     
+    //     $stmt->bind_param('ss', $username, $password);
+    //     $stmt->execute();
+    
+    //     $result = $stmt->get_result();
+    //     return $result->num_rows > 0;
+    // }
+
+    public function validateEmployee($username, $password)
+    {
+        $query = "SELECT * FROM employee WHERE username = ? AND password = ? AND accountActive = 1";
+        $stmt = $this->db->prepare($query);
+
+        if (!$stmt) {
+            die("SQL error: " . $this->db->error);
+        }
+
         $stmt->bind_param('ss', $username, $password);
         $stmt->execute();
-    
+
         $result = $stmt->get_result();
         return $result->num_rows > 0;
     }
 
+    public function signUpEmployee($username, $name,  $password,$dob)
+    {
+      
+        
+        // Insert query with a date format for DOB
+        $query = "INSERT INTO employee (username, name, password, dob, accountActive) 
+                  VALUES (?, ?, ?, ?, 0)"; // accountActive is set to 1 (active by default)
+        $stmt = $this->db->prepare($query);
+        
+        if (!$stmt) {
+            die("SQL error: " . $this->db->error);
+        }
+        
+        // Bind parameters
+        $stmt->bind_param('ssss', $username,$name, $password,  $dob);
+        $stmt->execute();
+        return $stmt->affected_rows > 0;
+    }
+    
     public function getAllEmployees()
     {
         $query = "SELECT username, name, password, DOB, deptId, accountActive FROM employee";
@@ -39,8 +74,6 @@ class EmployeeModel
         }
     }
 
-    
-
-    
+ 
 }
 ?>
